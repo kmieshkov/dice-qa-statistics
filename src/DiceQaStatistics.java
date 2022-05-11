ea1bc509ae78fca12da2a29d267ccb2674682835
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-05-11 15:42:22 -0400

    move all xpath to class attributes

--- src/DiceQaStatistics.java
+++ src/DiceQaStatistics.java
@@ -18,13 +18,19 @@ import java.util.*;
 
 public class DiceQaStatistics {
 
-	private static final String remoteDataBase = "jdbc:mysql://";
+	private static final String jdbc = "jdbc:mysql://";
 	private static String userName;
 	private static String password;
 	private static String db_url;
 	private static String url;
+	private static final By by_searchTerm = By.xpath("//input[@id='typeaheadInput']");
+	private static final By by_locator = By.id("google-location-search");
+	private static final By by_searchButton = By.id("submitSearch-button");
+	private static final String period7 = "//button[@data-cy-index='3']";
+	private static final String period1 = "//button[@data-cy-index='1']";
+	private static final By remoteOnly = By.xpath("//button[@aria-label='Filter Search Results by Remote Only']");
+	private static final By by_totalJobCount = By.id("totalJobCount");
 	public static Properties properties = new Properties();
-
 
 	private static void init() throws IOException {
 		properties.load(new FileInputStream("data.properties"));
@@ -37,7 +43,7 @@ public class DiceQaStatistics {
 	public static void main(String[] args) throws SQLException, IOException {
 		init();
 		// get search data from the DB
-		Connection con = DriverManager.getConnection(remoteDataBase + db_url, userName, password);
+		Connection con = DriverManager.getConnection(jdbc + db_url, userName, password);
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM `search_data`");
 		int i = 0;
@@ -64,7 +70,7 @@ public class DiceQaStatistics {
 		chromeOptions.addArguments("--disable-gpu");
 		chromeOptions.addArguments("--ignore-certificate-errors");
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("https://www.dice.com/");
+		driver.get(url);
 		//implicit wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -79,76 +85,71 @@ public class DiceQaStatistics {
 		driver.quit();
 	}
 
-	public static void workWithDB(Map<String, String> data) {
-		try {
-			Connection con = DriverManager.getConnection(remoteDataBase + db_url, userName, password);
-			Statement stmt = con.createStatement();
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `dice` (" +
-					"`id` INT(11) NOT NULL PRIMARY KEY auto_increment," +
-					"`job_title` VARCHAR(255)," +
-					"`location` VARCHAR(50)," +
-					"`period` VARCHAR(20)," +
-					"`result` VARCHAR(10)," +
-					"`date` DATE," +
-					"`time` TIME," +
-					"`week_day` VARCHAR(10)" +
-					");");
-			ResultSet rs = stmt.executeQuery("SELECT * FROM `dice` WHERE location = '" + data.get("location") + "' AND period = '" + data.get("period") + "' AND date = '" + LocalDate.now() + "'");
-			if (!rs.next()) {
-				PreparedStatement pstmt = con.prepareStatement("INSERT INTO `dice` (job_title, location, period, result, date, time, week_day) VALUES (?, ?, ?, ?, ?, ?, ?)");
-				pstmt.setString(1, data.get("job_title"));
-				pstmt.setString(2, data.get("location").equals("USA") ? "Remote Only" : data.get("location"));
-				pstmt.setString(3, data.get("period"));
-				pstmt.setString(4, data.get("result"));
-				pstmt.setString(5, java.time.LocalDate.now().toString());
-				pstmt.setString(6, java.time.LocalTime.now().toString());
-				pstmt.setString(7, LocalDate.parse(java.time.LocalDate.now().toString()).getDayOfWeek().toString());
-				pstmt.executeUpdate();
-			}
-			con.close();
-			System.out.println("Success: " + (data.get("location").equals("USA") ? "Remote Only" : data.get("location")) + " - " + data.get("period"));
+	public static void workWithDB(Map<String, String> data) throws SQLException {
+		Connection con = DriverManager.getConnection(jdbc + db_url, userName, password);
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `dice` (" +
+				"`id` INT(11) NOT NULL PRIMARY KEY auto_increment," +
+				"`job_title` VARCHAR(255)," +
+				"`location` VARCHAR(50)," +
+				"`period` VARCHAR(20)," +
+				"`result` VARCHAR(10)," +
+				"`date` DATE," +
+				"`time` TIME," +
+				"`week_day` VARCHAR(10)" +
+				");");
+		ResultSet rs = stmt.executeQuery("SELECT * FROM `dice` WHERE location = '" + data.get("location") + "' AND period = '" + data.get("period") + "' AND date = '" + LocalDate.now() + "'");
+		if (!rs.next()) {
+			PreparedStatement pstmt = con.prepareStatement("INSERT INTO `dice` (job_title, location, period, result, date, time, week_day) VALUES (?, ?, ?, ?, ?, ?, ?)");
+			pstmt.setString(1, data.get("job_title"));
+			pstmt.setString(2, data.get("location").equals("USA") ? "Remote Only" : data.get("location"));
+			pstmt.setString(3, data.get("period"));
+			pstmt.setString(4, data.get("result"));
+			pstmt.setString(5, java.time.LocalDate.now().toString());
+			pstmt.setString(6, java.time.LocalTime.now().toString());
+			pstmt.setString(7, LocalDate.parse(java.time.LocalDate.now().toString()).getDayOfWeek().toString());
+			pstmt.executeUpdate();
 		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+		con.close();
+		System.out.println("Success: " + (data.get("location").equals("USA") ? "Remote Only" : data.get("location")) + " - " + data.get("period"));
 	}
 
 	public static String script(WebDriver driver, Map<String, String> data) {
 
 		// set Job title
-		WebElement searchTerm = driver.findElement(By.xpath("//input[@id='typeaheadInput']"));
+		WebElement searchTerm = driver.findElement(by_searchTerm);
 		searchTerm.clear();
 		searchTerm.sendKeys(data.get("job_title"));
 
 		// set Location
-		WebElement locator = driver.findElement(By.id("google-location-search"));
+		WebElement locator = driver.findElement(by_locator);
 		locator.clear();
 		locator.sendKeys(data.get("location"));
 
 		// click Search button
-		WebElement searchButton = driver.findElement(By.id("submitSearch-button"));
+		WebElement searchButton = driver.findElement(by_searchButton);
 		searchButton.click();
 
 		// click filter Last 7 Days
 		String xpathPeriod;
 		if (data.get("period").equals("7")) {
-			xpathPeriod = "//button[@data-cy-index='3']";
+			xpathPeriod = period7;
 		} else {
-			xpathPeriod = "//button[@data-cy-index='1']";
+			xpathPeriod = period1;
 		}
 		WebElement period = driver.findElement(By.xpath(xpathPeriod));
 		period.click();
 
 		// if location USA -> set REMOTE ONLY OPTION
 		if (data.get("location").equals("USA")) {
-			driver.findElement(By.xpath("//button[@aria-label='Filter Search Results by Remote Only']")).click();
+			driver.findElement(remoteOnly).click();
 		}
 
 		// reload page
 		driver.navigate().refresh();
 
 		// Get Total Job Count
-		WebElement totalJobCount = driver.findElement(By.id("totalJobCount"));
+		WebElement totalJobCount = driver.findElement(by_totalJobCount);
 		String result = totalJobCount.getText().replace(",", "");
 
 		driver.navigate().to(url);
